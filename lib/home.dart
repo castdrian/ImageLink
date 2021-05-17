e0f2464e962041ab25c280906c17b4e3f7b3a8ae
adrifcastr <adrifcastr@gmail.com>
2021-05-17 14:36:23 +0200

    fix possible crash

--- lib/home.dart
+++ lib/home.dart
@@ -17,42 +17,43 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(children: <Widget>[
-          SizedBox(height: 180),
-          Expanded(child: fileMedia == null ? Icon(Icons.photo, size: 120) : Image.file(fileMedia)),
-          SizedBox(height: 140),
-          Container(
-            width: double.infinity,
-            height: 70.0,
-            child: OutlinedButton.icon(
-              label: Text('Select Image', style: TextStyle(fontSize: 30)),
-              icon: Icon(Icons.image, size: 30),
-              onPressed: () => pickGalleryMedia(context),
-            ),
-          ),
-          SizedBox(height: 24),
-          Container(
-            width: double.infinity,
-            height: 70.0,
-            child: OutlinedButton.icon(
-              label: Text('Upload', style: TextStyle(fontSize: 30)),
-              icon: Icon(Icons.upload_file, size: 30),
-              onPressed: () {
-                Fluttertoast.showToast(
-                    msg: "Uploading Image...",
-                    toastLength: Toast.LENGTH_SHORT,
-                    timeInSecForIosWeb: 2,
-                    backgroundColor: Colors.red,
-                    textColor: Colors.white,
-                    fontSize: 16.0
-                );
-              },
-            ),
-          ),
+      SizedBox(height: 180),
+      Expanded(
+          child: fileMedia == null
+              ? Icon(Icons.photo, size: 120)
+              : Image.file(fileMedia)),
+      SizedBox(height: 140),
+      Container(
+        width: double.infinity,
+        height: 70.0,
+        child: OutlinedButton.icon(
+          label: Text('Select Image', style: TextStyle(fontSize: 30)),
+          icon: Icon(Icons.image, size: 30),
+          onPressed: () => pickGalleryMedia(context),
+        ),
+      ),
+      SizedBox(height: 24),
+      Container(
+        width: double.infinity,
+        height: 70.0,
+        child: OutlinedButton.icon(
+          label: Text('Upload', style: TextStyle(fontSize: 30)),
+          icon: Icon(Icons.upload_file, size: 30),
+          onPressed: () {
+            Fluttertoast.showToast(
+                msg: "Uploading Image...",
+                toastLength: Toast.LENGTH_SHORT,
+                timeInSecForIosWeb: 2,
+                fontSize: 16.0);
+          },
+        ),
+      ),
     ]));
   }
 
   Future pickGalleryMedia(BuildContext context) async {
     final media = await ImagePicker().getImage(source: ImageSource.gallery);
+    if (media == null) return;
     final file = File(media.path);
 
     if (file == null) {
