ea87ce717ab7bbc08b8fd841f9ea3cb8bc775a9b
adrifcastr <adrifcastr@gmail.com>
2021-05-17 12:52:52 +0200

    some buttons

--- lib/donate.dart
+++ lib/donate.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Donate extends StatefulWidget {
   @override
@@ -10,8 +11,58 @@ class Donate extends StatefulWidget {
 class _DonateState extends State<Donate> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return Scaffold(body: Column(children: <Widget>[
+      SizedBox(height: 14),
+        Container(
+          width: double.infinity,
+          height: 70.0,
+          child: OutlinedButton.icon(
+            label: Text('Paypal', style: TextStyle(fontSize: 25)),
+            icon: Icon(FontAwesomeIcons.paypal, size: 30),
+            onPressed: () {},
+          ),
+        ),
+        SizedBox(height: 24),
+        Container(
+            width: double.infinity,
+            height: 70.0,
+            child: OutlinedButton.icon(
+              label: Text('Patreon', style: TextStyle(fontSize: 25)),
+              icon: Icon(FontAwesomeIcons.patreon, size: 30),
+              onPressed: () {},
+            ),
+          ),
+        SizedBox(height: 164),
+        Container(
+            width: double.infinity,
+            height: 70.0,
+            child: OutlinedButton.icon(
+              label: Text('GitHub', style: TextStyle(fontSize: 25)),
+              icon: Icon(FontAwesomeIcons.github, size: 30),
+              onPressed: () {},
+            ),
+          ),
+        SizedBox(height: 24),
+        Container(
+          width: double.infinity,
+          height: 70.0,
+          child: OutlinedButton.icon(
+            label: Text('Discord', style: TextStyle(fontSize: 25)),
+            icon: Icon(FontAwesomeIcons.discord, size: 30),
+            onPressed: () {},
+          ),
+        ),
+        SizedBox(height: 24),
+        Container(
+          width: double.infinity,
+          height: 70.0,
+          child: OutlinedButton.icon(
+            label: Text('Twitter', style: TextStyle(fontSize: 25)),
+            icon: Icon(FontAwesomeIcons.twitter, size: 30),
+            onPressed: () {},
+          ),
+        ),
+      ])
     );
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
- @override
- State<StatefulWidget> createState() {
+  @override
+  State<StatefulWidget> createState() {
     return _HomeState();
   }
 }
 
 class _HomeState extends State<Home> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+
     );
   }
 }
